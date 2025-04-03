45afdec41113c03f007ca5f7d1b8234ff4105dd2
Mauro Faria <m.r.d.faria@gmail.com>
2025-04-03 23:34:09 +0200

    Got rid  of crafted

--- init.el
+++ init.el
@@ -1,81 +1,176 @@
-(cua-mode)
-
-
 ;; Set up custom.el file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (and custom-file
            (file-exists-p custom-file))
   (load custom-file nil :nomessage))
 
-;; Bootstrap crafted-emacs in init.el
-(load "~/crafted-emacs/modules/crafted-init-config")
+;; No welcome screen - opens directly in scratch buffer
+(setq inhibit-startup-message t
+      initial-scratch-message "Hello there Mauro"
+      initial-major-mode 'fundamental-mode
+      inhibit-splash-screen t)
 
+;; Remove annoying bell
+(setq visible-bell nil)
+(setq ring-bell-function 'ignore)
+(cua-mode 1)
+
+;; Increase large file warning
+(setq large-file-warning-threshold (* 15 1024 1024))
+
+;; Lazy prompting
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Set up package priorities
 (with-eval-after-load 'package
   (setq package-archive-priorities
-        '(("gnu" . 15)          ;; Highest priority
-          ("nongnu" . 10)       ;; Second priority
-          ("melpa" . 5)         ;; Lower than gnu/nongnu
-          ("melpa-stable" . 1)))) ;; Lowest priority
+        '(("gnu" . 10)          ;; Highest priority
+          ("nongnu" . 5)       ;; Second priority
+          ("melpa" . 15)         ;; Lower than gnu/nongnu
+          ("melpa-stable" . 0)))) ;; Lowest priority, i know, but packages are pretty old
 
+;; Make sure use package is installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
 
-(require 'crafted-ide-packages)
-(require 'crafted-ui-packages)
-(require 'crafted-org-packages)
-(require 'crafted-completion-packages)
-
-;; Install the packages listed in the `package-selected-packages' list.
-(add-to-list 'package-selected-packages 'ef-themes)
-(add-to-list 'package-selected-packages 'gdscript-mode)
-(add-to-list 'package-selected-packages 'denote-journal)
-(add-to-list 'package-selected-packages 'magit)
-(package-install-selected-packages :noconfirm)
-
-;; Load configuration
-(require 'recentf)
-(require 'crafted-completion-config)
-
-;; IDE and language completion
-(require 'gdscript-mode)
-(setq treesit-language-source-alist
-      '((python "https://github.com/tree-sitter/tree-sitter-python")
-        (gdscript "https://github.com/PrestonKnopp/tree-sitter-gdscript")))
-(require 'crafted-ide-config)
-(crafted-ide-eglot-auto-ensure-all)
-(crafted-ide-configure-tree-sitter)
-
-;; Load crafted-startup configuration
-(require 'crafted-startup-config)
-
-;; Load crafted-ui configuration
-(require 'crafted-ui-config)
-
-;; Org mode config
-(require 'crafted-org-config)
-(use-package denote
+;; Slurp environment variables from the shell.
+;; a.k.a. The Most Asked Question On r/emacs
+(use-package exec-path-from-shell
   :ensure t
-  :hook (dired-mode . denote-dired-mode)
-  :bind
-  (("C-c n n" . denote)
-   ("C-c n r" . denote-rename-file)
-   ("C-c n l" . denote-link)
-   ("C-c n b" . denote-backlinks)
-   ("C-c n d" . denote-sort-dired))
   :config
-  (setq denote-directory (expand-file-name "~/Documents/notes/"))
-  (denote-rename-buffer-mode 1))
+  (exec-path-from-shell-initialize))
 
-;; Load crafted-updates configuration
-(require 'crafted-defaults-config)
+;; Fido (icomplete now includes fuzzy matching for M-x and other completions)
+(use-package icomplete
+  :bind (:map icomplete-minibuffer-map
+              ("C-n" . icomplete-forward-completions)
+              ("C-p" . icomplete-backward-completions)
+              ("C-v" . icomplete-vertical-toggle)
+              ("RET" . icomplete-force-complete-and-exit))
+  :hook
+  (after-init . (lambda ()
+                  (fido-mode -1)
+                  ;; (icomplete-mode 1)
+                  (icomplete-vertical-mode 1)
+                  ))
+  :config
+  (setq tab-always-indent 'complete)  ;; Starts completion with TAB
+  (setq icomplete-delay-completions-threshold 0)
+  (setq icomplete-compute-delay 0)
+  (setq icomplete-show-matches-on-no-input t)
+  (setq icomplete-hide-common-prefix nil)
+  (setq icomplete-prospects-height 10)
+  (setq icomplete-separator " . ")
+  (setq icomplete-with-completion-tables t)
+  (setq icomplete-in-buffer t)
+  (setq icomplete-max-delay-chars 0)
+  (setq icomplete-scroll t)
+  (advice-add 'completion-at-point
+              :after #'minibuffer-hide-completions))
 
-;; Load crafted-speedbar configuration
-(require 'crafted-speedbar-config)
-(customize-set-variable 'speedbar-use-images t)
+;; Recent Files
+(use-package recentf
+  :config
+  (setq recentf-auto-cleanup 'never) ;; prevent issues with Tramp
+  (setq recentf-max-saved-items 100)
+  (setq recentf-max-menu-items 15)
+  (recentf-mode t)
 
-;; Escape key 
+  (defun my/recentf-ido-find-file ()
+    "Find a recent file using ido."
+    (interactive)
+    (let ((file (completing-read "Choose recent file: " recentf-list nil t)))
+      (when file
+        (find-file file))))
+
+  :bind ("C-x f" . my/recentf-ido-find-file))
+
+;; Manage the browser
+(use-package eww
+  :bind* (("M-m g x" . eww)
+          ("M-m g :" . eww-browse-with-external-browser)
+          ("M-m g #" . eww-list-histories)
+          ("M-m g {" . eww-back-url)
+          ("M-m g }" . eww-forward-url))
+  :config
+  (progn
+    (add-hook 'eww-mode-hook 'visual-line-mode)))
+
+;; Restart emacs from emacs
+(use-package restart-emacs
+  :ensure t
+  :bind* (("C-x M-c" . restart-emacs)))
+
+;; Useful for remembering chord
+(use-package which-key
+  :ensure t
+  :defer t
+  :diminish which-key-mode
+  :init
+  (setq which-key-sort-order 'which-key-key-order-alpha)
+  :bind* (("M-m ?" . which-key-show-top-level))
+  :config
+  (which-key-mode)
+  (which-key-add-key-based-replacements
+   "M-m ?" "top level bindings"))
+
+;; Fleeting notes
+(use-package org
+  :after
+  denote
+  :bind
+  (("C-c c" . org-capture)
+   ("C-c l" . org-store-link))
+  :custom
+  (setq org-image-actual-width 200)
+  (org-default-notes-file ews-inbox-file)
+  (org-capture-bookmark nil)
+  ;; Capture templates
+  (org-capture-templates
+   '(("f" "Fleeting note" item
+      (file+headline org-default-notes-file "Notes")
+      "- %?")
+     ("p" "Permanent note" plain
+      (file denote-last-path)
+      #'denote-org-capture
+      :no-save t
+      :immediate-finish nil
+      :kill-buffer t
+      :jump-to-captured t)
+     ("t" "New task" entry
+      (file+headline org-default-notes-file "Tasks")
+      "* TODO %i%?"))))
+
+;; Markdown mode
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\.markdown\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+              ("C-c C-e" . markdown-do)))
+
+;; Treesit and languages
+(setq treesit-language-source-alist
+     '((python "https://github.com/tree-sitter/tree-sitter-python")
+       (gdscript "https://github.com/PrestonKnopp/tree-sitter-gdscript")
+       (bash "https://github.com/tree-sitter/tree-sitter-bash")))
+
+;; Godot
+(use-package gdscript-mode
+  :ensure t
+  :hook (gdscript-mode . eglot-ensure))
+
+(add-to-list 'major-mode-remap-alist
+	     '(gdscript-mode . gdscript-ts-mode)
+	     '(sh-mode . bash-ts-mode))
+
+;; Make escape key stronger
 (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-
-
