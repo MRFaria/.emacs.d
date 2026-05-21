e73d26398da6cab62443a5a8d6ce04410683b3fa
Mauro Faria <mauro.faria@xfel.eu>
2026-05-21 16:01:07 +0200

    Removed themes

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
   (setq org-journal-enable-agenda-integration t)
   (setq org-journal-dir note-directory)
   (setq org-journal-date-format "%A, %Y-%m-%d")
-  (setq org-journal-file-type 'weekly)
+  (setq org-journal-file-type 'daily)
   ;;(setq org-journal-file-format "%Y%m%d__Week-%V-%Y__journal.org")
   :bind
   ("<f7>" . org-journal-new-entry))
@@ -138,30 +138,4 @@
 
 (use-package pyvenv
   :ensure t)
-
-(use-package standard-themes
-  :ensure t
-  :init
-  ;; This makes the Modus commands listed below consider only the Ef
-  ;; themes.  For an alternative that includes Modus and all
-  ;; derivative themes (like Ef), enable the
-  ;; `modus-themes-include-derivatives-mode' instead.  The manual of
-  ;; the Ef themes has a section that explains all the possibilities:
-  ;;
-  ;; - Evaluate `(info "(standard-themes) Working with other Modus themes or taking over Modus")'
-  ;; - Visit <https://protesilaos.com/emacs/standard-themes#h:d8ebe175-cd61-4e0b-9b84-7a4f5c7e09cd>
-  (standard-themes-take-over-modus-themes-mode 1)
-  :bind
-  (("<f5>" . modus-themes-rotate)
-   ("C-<f5>" . modus-themes-select)
-   ("M-<f5>" . modus-themes-load-random))
-  :config
-  ;; All customisations here.
-  (setq modus-themes-mixed-fonts t)
-  (setq modus-themes-italic-constructs t)
-
-  ;; Finally, load your theme of choice (or a random one with
-  ;; `modus-themes-load-random', `modus-themes-load-random-dark',
-  ;; `modus-themes-load-random-light').
-  (modus-themes-load-theme 'standard-light-tinted))
 
